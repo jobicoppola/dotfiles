57506dec600efe637aa13a8a6f42521e0f7e54fd
jcopp <jcopp@sportingnews.com>
2012-04-07 04:32:02 -0400

    Don't rsync iterm colors

--- configurationalize.sh
+++ configurationalize.sh
@@ -9,7 +9,12 @@ read -p "NOTE: This can/will dropkick existing files in $HOME - proceed? (y/n) "
 
 if [[ $yaynay == y* ]]; then
     echo -e "\nSyncing dotfiles repo with home dir...\n"
-    rsync --exclude ".git/" --exclude "README.md" --exclude "configurationalize.sh" --exclude ".DS_Store" -av . ~
+    rsync --exclude ".git/" \
+        --exclude "README.md" \
+        --exclude "configurationalize.sh" \
+        --exclude ".DS_Store" \
+        --exclude "macbot.itermcolors" \ 
+        -av . ~
 fi
 
 . "$HOME/.bashrc"
